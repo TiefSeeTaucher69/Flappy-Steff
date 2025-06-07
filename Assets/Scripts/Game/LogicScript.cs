d0142c1217d4c2781a736682d86e3fc492cd7d24
TiefSeeTaucher69 <diepersdorf88@gmail.com>
2025-06-07 12:43:29 +0200

    added Leaderboard

--- Assets/Scripts/Game/LogicScript.cs
+++ Assets/Scripts/Game/LogicScript.cs
@@ -9,6 +9,8 @@ public class LogicScript : MonoBehaviour
     public Text scoreText;
     public GameObject gameOverScreen;
     public GameObject menuScreen;
+    public LeaderboardSenderScript leaderboardSenderScript;
+    private bool hasGameOverBeenHandled = false;
 
     [ContextMenu("Increase Score")]
     public void addScore(int scoreToAdd)
@@ -24,13 +26,20 @@ public class LogicScript : MonoBehaviour
 
     public void gameOver()
     {
+        if (hasGameOverBeenHandled) return;
+        hasGameOverBeenHandled = true;
+
         gameOverScreen.SetActive(true);
+
         if (playerScore > highScore)
         {
             PlayerPrefs.SetInt("Highscore", playerScore);
             PlayerPrefs.Save();
             Debug.Log("New high score saved: " + playerScore);
-        } else
+            string username = PlayerPrefs.GetString("Username", "Anonymous");
+            StartCoroutine(leaderboardSenderScript.SendScore(username, playerScore));
+        }
+        else
         {
             Debug.Log("Kein neuer Highscore. Aktueller: " + highScore);
         }
@@ -45,5 +54,6 @@ public class LogicScript : MonoBehaviour
     private void Start()
     {
         highScore = PlayerPrefs.GetInt("Highscore", 0);
+        leaderboardSenderScript = GameObject.Find("LeaderboardSender").GetComponent<LeaderboardSenderScript>();
     }
 }
