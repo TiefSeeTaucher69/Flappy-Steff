ebbaee33f17d32514e01e0cb20e4f449eee6c24e
TiefSeeTaucher69 <diepersdorf88@gmail.com>
2025-06-07 18:47:37 +0200

    implemented Resolution changing in Settings

--- Assets/Scripts/BootScene/BootSceneScript.cs
+++ Assets/Scripts/BootScene/BootSceneScript.cs
@@ -3,10 +3,31 @@ using UnityEngine.SceneManagement;
 
 public class BootSceneScript : MonoBehaviour
 {
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         QualitySettings.vSyncCount = 0; // Deaktiviere VSync, um FPS Cap zu ermöglichen
+
+        // Auflösung laden oder Standard (native)
+        int resIndex = PlayerPrefs.GetInt("ResolutionIndex", -1);
+        if (resIndex != -1)
+        {
+            Resolution[] resolutions = Screen.resolutions;
+            if (resIndex < resolutions.Length)
+            {
+                Resolution res = resolutions[resIndex];
+                Screen.SetResolution(res.width, res.height, FullScreenMode.FullScreenWindow, res.refreshRate);
+                Debug.Log("Auflösung geladen aus PlayerPrefs: " + res.width + "x" + res.height);
+            }
+        }
+        else
+        {
+            // Keine gespeicherte Auflösung → native Auflösung setzen
+            Resolution nativeRes = Screen.currentResolution;
+            Screen.SetResolution(nativeRes.width, nativeRes.height, true);
+            Debug.Log("Native Auflösung gesetzt: " + nativeRes.width + "x" + nativeRes.height);
+        }
 
         int fpsIndex = PlayerPrefs.GetInt("FPSCap", 4); // Default Index 4 (240 FPS)
 
--- Assets/Scripts/Game/VideoSettingsInGameScript.cs
+++ Assets/Scripts/Game/VideoSettingsInGameScript.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,8 @@ public class VideoSettingsInGameScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Dropdown fpsDropdown; // Dropdown für FPS Cap
     public const string PlayerPrefsKey = "FPSCap"; // Schlüssel für PlayerPrefs
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,6 +32,35 @@ public class VideoSettingsInGameScript : MonoBehaviour
 
         // Listener wieder hinzufügen
         fpsDropdown.onValueChanged.AddListener(OnDropdownChanged);
+
+
+
+        resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        int currentResIndex = 0;
+        var options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height &&
+                resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = PlayerPrefs.GetInt("ResolutionIndex", currentResIndex);
+        resolutionDropdown.RefreshShownValue();
+
+        ApplyResolution(resolutionDropdown.value);
+
+        resolutionDropdown.onValueChanged.AddListener(OnResolutionChanged);
     }
 
 
@@ -68,5 +100,19 @@ public class VideoSettingsInGameScript : MonoBehaviour
                 Application.targetFrameRate = 240; // Fallback unbegrenzt
                 break;
         }
+    }
+
+    public void OnResolutionChanged(int index)
+    {
+        ApplyResolution(index);
+        PlayerPrefs.SetInt("ResolutionIndex", index);
+        PlayerPrefs.Save();
+    }
+
+    void ApplyResolution(int index)
+    {
+        Resolution res = resolutions[index];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreenMode, res.refreshRate);
+        Debug.Log("Auflösung gesetzt auf: " + res.width + "x" + res.height);
     }
 }
